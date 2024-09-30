0bc98b447018e8c96ecc335f9548adcc4b188ac0
charlie-flynn <charlie.flynn.business@gmail.com>
2024-09-30 10:35:38 -0500

    Initial commit

---
+++ SerializeIO.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExercises
+{
+    struct Contact
+    {
+        public string name;
+        public string email;
+        public int id;
+        public Contact(string name, string email, int id)
+        {
+            this.name = name;
+            this.email = email;
+            this.id = id;
+        }
+        public void Print()
+        {
+            Console.WriteLine(name + " | " + email + " | " + id);
+        }
+        public void Serialize(string path)
+        {
+
+        }
+        public void DeSerialize(string path)
+        {
+
+        }
+    }
+    internal class SerializeIO
+    {
+        public void Run()
+        {
+            // make 3 contacts
+            Contact bob = new Contact("Bob", "bobrobloxgaming@gmail.com", 1234);
+            Contact jane = new Contact("Jane", "janessica.jove.business@gmail.com", 2345);
+            Contact fred = new Contact("Fred", "freddyfivebears@gmail.com", 5555);
+
+            // write each contact to a file
+            bob.Serialize(@"contacts\bob.txt");
+            fred.Serialize(@"contacts\fred.txt");
+            jane.Serialize(@"contacts\jane.txt");
+
+            // clear out contacts
+            bob = new Contact();
+            jane = new Contact();
+            fred = new Contact();
+
+            // load contacts from file
+            bob.DeSerialize(@"contacts\bob.txt");
+            fred.DeSerialize(@"contacts\fred.txt");
+            jane.DeSerialize(@"contacts\jane.txt");
+
+            // print contacts
+            bob.Print();
+            fred.Print();
+            jane.Print();
+        }
+    }
+}
---
+++ TextIO.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExercises
+{
+    internal class TextIO
+    {
+        public void Run()
+        {
+            string path = "test.txt";
+
+            // Write my awesome file
+            if (!File.Exists(path))
+            {
+                try
+                {
+                    StreamWriter writer = new StreamWriter(path, false);
+                    writer.WriteLine("Hello");
+                    writer.WriteLine("And");
+                    writer.WriteLine("Welcome! (awesome)");
+                    writer.Close();
+
+                    // dispose of the awesome object
+                    try
+                    {
+                        writer.Dispose();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+            }
+
+            // Read my awesome file
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string str;
+
+                    while ((str = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
